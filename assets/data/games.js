8c49103bedac5b207c73feb5ec024571a1d84aa6
YakultIpramoavic <146249445+YakultIpramoavic@users.noreply.github.com>
2025-12-02 23:33:29 +0700

    Update games.js

--- assets/data/games.js
+++ assets/data/games.js
@@ -7,7 +7,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]", "[edit]"]
+    features: []
   },
   {
     id: 102,
@@ -17,17 +17,17 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]", "[edit]"]
+    features: []
   },
   {
     id: 103,
     name: "CO-OP TD",
     description: "Gold In Battle – Stone In Battle",
     image: "assets/img/game_103.png",
-    script: "https://t.t.me/YakultIpramovic",
-    download: "https://t.t.me/YakultIpramovic",
-    video: "https://t.t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]", "[edit]"]
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
   },
   {
     id: 104,
@@ -37,7 +37,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]"]
+    features: []
   },
   {
     id: 105,
@@ -47,7 +47,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]"]
+    features: []
   },
   {
     id: 106,
@@ -57,7 +57,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]", "[edit]"]
+    features: []
   },
   {
     id: 107,
@@ -67,7 +67,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]"]
+    features: []
   },
   {
     id: 108,
@@ -77,7 +77,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]"]
+    features: []
   },
   {
     id: 109,
@@ -87,7 +87,7 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]"]
+    features: []
   },
   {
     id: 110,
@@ -97,9 +97,8 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]"]
+    features: []
   },
-
   {
     id: 111,
     name: "LEGEND HERO",
@@ -108,8 +107,204 @@ const gamesData = [
     script: "https://t.me/YakultIpramovic",
     download: "https://t.me/YakultIpramovic",
     video: "https://t.me/YakultIpramovic",
-    features: ["[edit]", "[edit]"]
+    features: []
   },
   {
     id: 112,
     name: "IMAE GUARDIAN GIRL",
+    description: "Free Shop",
+    image: "assets/img/game_112.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 113,
+    name: "LEGENDARY GIRL WARRIOR IDLE",
+    description: "Free Shop",
+    image: "assets/img/game_113.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 114,
+    name: "LUCIFER IDLE",
+    description: "Free Shop",
+    image: "assets/img/game_114.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 115,
+    name: "PIXEL HEROIC LEGEND IDLE RPG",
+    description: "Free Shop",
+    image: "assets/img/game_115.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 116,
+    name: "RAISING POSEIDON IDLE RPG",
+    description: "Free Shop",
+    image: "assets/img/game_116.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 117,
+    name: "IMMORTAL RISING",
+    description: "Free Shop",
+    image: "assets/img/game_117.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 118,
+    name: "SLIME HUNTER WILD PATCH",
+    description: "Free Shop",
+    image: "assets/img/game_118.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 119,
+    name: "SWITCHING HEROES IDLE",
+    description: "Free Shop",
+    image: "assets/img/game_119.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 120,
+    name: "SWORD LEGEND IDLE RPG",
+    description: "Free Shop",
+    image: "assets/img/game_120.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 121,
+    name: "ZOMBIE KINGDOM IDLE RPG",
+    description: "Free Shop",
+    image: "assets/img/game_121.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 122,
+    name: "VALKYRIE IDLE",
+    description: "Free Shop Costume – Drop Item – Unlimited Ruby – Unlock Pass",
+    image: "assets/img/game_122.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 123,
+    name: "TAILED DEMON SLAYER RISE",
+    description: "Antiban – Diamond – Skip Tutorial – Unlimited Currency",
+    image: "assets/img/game_123.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 124,
+    name: "NINJA DEFENSE",
+    description: "Free Shop",
+    image: "assets/img/game_124.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 125,
+    name: "AFK THREEKINGDOM IDLE RPG",
+    description: "Antiban – Free Shop – Damage – No Skill CD – Hack Resources",
+    image: "assets/img/game_125.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 126,
+    name: "CASTLE DEFENSE OATH KEEPERS TD",
+    description: "Free Shop – Multi Reward",
+    image: "assets/img/game_126.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 127,
+    name: "CAT HERO",
+    description: "Antiban – Hack Resources",
+    image: "assets/img/game_127.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 128,
+    name: "CROWN RUSH",
+    description: "Antiban – Free Shop – Hack Resources",
+    image: "assets/img/game_128.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 129,
+    name: "DELICIOUS MONSTERS TD TYCOON",
+    description: "Antiban – Free Shop",
+    image: "assets/img/game_129.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 130,
+    name: "DOPAMINE HIT",
+    description: "Antiban – Free Shop – Gold/Exp/Silver Drop",
+    image: "assets/img/game_130.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
+  },
+  {
+    id: 131,
+    name: "FORTIAS SAGA IDLE RPG",
+    description: "Antiban – Free Shop",
+    image: "assets/img/game_131.png",
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: []
