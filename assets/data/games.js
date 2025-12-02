1b4c2bd9b3292fd09c75942341a9b10d0f6234fe
YakultIpramoavic <146249445+YakultIpramoavic@users.noreply.github.com>
2025-12-02 23:24:02 +0700

    Update games.js

--- assets/data/games.js
+++ assets/data/games.js
@@ -4,492 +4,112 @@ const gamesData = [
     name: "PRITIVEMER BROTHER",
     description: "Antiban – Attack Speed – Damage – Diamond – Box Lv300 + Tier",
     image: "assets/img/game_101.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]", "[edit]"]
   },
   {
     id: 102,
     name: "CASTLE DEFENSE OATH KEEPERS TD",
     description: "Damage – Free Shop – Multi Reward",
     image: "assets/img/game_102.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]", "[edit]"]
   },
   {
     id: 103,
     name: "CO-OP TD",
     description: "Gold In Battle – Stone In Battle",
     image: "assets/img/game_103.png",
-    script: ""
+    script: "https://t.t.me/YakultIpramovic",
+    download: "https://t.t.me/YakultIpramovic",
+    video: "https://t.t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]", "[edit]"]
   },
   {
     id: 104,
     name: "CROWN SAGA",
     description: "Free Shop",
     image: "assets/img/game_104.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]"]
   },
   {
     id: 105,
     name: "DEAD IMPACT",
     description: "Free Shop",
     image: "assets/img/game_105.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]"]
   },
   {
     id: 106,
     name: "DARK CLAN IDLE RPG",
     description: "Antiban – Free Shop – Gold Drop – Attack Speed – No Skill CD",
     image: "assets/img/game_106.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]", "[edit]"]
   },
   {
     id: 107,
     name: "DEMON HUNTER IDLE",
     description: "Free Package",
     image: "assets/img/game_107.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]"]
   },
   {
     id: 108,
     name: "GODCHER",
     description: "Antiban – Gold/Exp/Stone Drop – Currency – Skip ADS",
     image: "assets/img/game_108.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]"]
   },
   {
     id: 109,
     name: "GROW DEMON SLIME",
     description: "Gold – Exp – Stone Drop",
     image: "assets/img/game_109.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]"]
   },
   {
     id: 110,
     name: "HUNTER RAID",
     description: "Free Shop",
     image: "assets/img/game_110.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]"]
   },
+
   {
     id: 111,
     name: "LEGEND HERO",
     description: "Antiban – Gold/Exp/Stone Drop – Free Shop",
     image: "assets/img/game_111.png",
-    script: ""
+    script: "https://t.me/YakultIpramovic",
+    download: "https://t.me/YakultIpramovic",
+    video: "https://t.me/YakultIpramovic",
+    features: ["[edit]", "[edit]"]
   },
   {
     id: 112,
     name: "IMAE GUARDIAN GIRL",
-    description: "Free Shop",
-    image: "assets/img/game_112.png",
-    script: ""
-  },
-  {
-    id: 113,
-    name: "LEGENDARY GIRL WARRIOR IDLE",
-    description: "Free Shop",
-    image: "assets/img/game_113.png",
-    script: ""
-  },
-  {
-    id: 114,
-    name: "LUCIFER IDLE",
-    description: "Free Shop",
-    image: "assets/img/game_114.png",
-    script: ""
-  },
-  {
-    id: 115,
-    name: "PIXEL HEROIC LEGEND IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_115.png",
-    script: ""
-  },
-  {
-    id: 116,
-    name: "RAISING POSEIDON IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_116.png",
-    script: ""
-  },
-  {
-    id: 117,
-    name: "IMMORTAL RISING",
-    description: "Free Shop",
-    image: "assets/img/game_117.png",
-    script: ""
-  },
-  {
-    id: 118,
-    name: "SLIME HUNTER WILD PATCH",
-    description: "Free Shop",
-    image: "assets/img/game_118.png",
-    script: ""
-  },
-  {
-    id: 119,
-    name: "SWITCHING HEROES IDLE",
-    description: "Free Shop",
-    image: "assets/img/game_119.png",
-    script: ""
-  },
-  {
-    id: 120,
-    name: "SWORD LEGEND IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_120.png",
-    script: ""
-  },
-  {
-    id: 121,
-    name: "ZOMBIE KINGDOM IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_121.png",
-    script: ""
-  },
-  {
-    id: 122,
-    name: "VALKYRIE IDLE",
-    description: "Free Shop Costume – Drop Item – Unlimited Ruby – Unlock Pass",
-    image: "assets/img/game_122.png",
-    script: ""
-  },
-  {
-    id: 123,
-    name: "TAILED DEMON SLAYER RISE",
-    description: "Antiban – Diamond – Skip Tutorial – Unlimited Currency",
-    image: "assets/img/game_123.png",
-    script: ""
-  },
-  {
-    id: 124,
-    name: "NINJA DEFENSE",
-    description: "Free Shop",
-    image: "assets/img/game_124.png",
-    script: ""
-  },
-  {
-    id: 125,
-    name: "AFK THREEKINGDOM IDLE RPG",
-    description: "Antiban – Free Shop – Damage – No Skill CD – Hack Resources",
-    image: "assets/img/game_125.png",
-    script: ""
-  },
-  {
-    id: 126,
-    name: "CASTLE DEFENSE OATH KEEPERS TD",
-    description: "Free Shop – Multi Reward",
-    image: "assets/img/game_126.png",
-    script: ""
-  },
-  {
-    id: 127,
-    name: "CAT HERO",
-    description: "Antiban – Hack Resources",
-    image: "assets/img/game_127.png",
-    script: ""
-  },
-  {
-    id: 128,
-    name: "CROWN RUSH",
-    description: "Antiban – Free Shop – Hack Resources",
-    image: "assets/img/game_128.png",
-    script: ""
-  },
-  {
-    id: 129,
-    name: "DELICIOUS MONSTERS TD TYCOON",
-    description: "Antiban – Free Shop",
-    image: "assets/img/game_129.png",
-    script: ""
-  },
-  {
-    id: 130,
-    name: "DOPAMINE HIT",
-    description: "Antiban – Free Shop – Gold/Exp/Silver Drop",
-    image: "assets/img/game_130.png",
-    script: ""
-  },
-  {
-    id: 131,
-    name: "FORTIAS SAGA IDLE RPG",
-    description: "Antiban – Free Shop",
-    image: "assets/img/game_131.png",
-    script: ""
-  },
-  {
-    id: 132,
-    name: "CROWN RUSH",
-    description: "Antiban – Free Shop – Hack Resources",
-    image: "assets/img/game_132.png",
-    script: ""
-  },
-  {
-    id: 133,
-    name: "GHOST HUNTER IDLE",
-    description: "Antiban – Free Shop – Gold/Exp Drop",
-    image: "assets/img/game_133.png",
-    script: ""
-  },
-  {
-    id: 134,
-    name: "GROW DEMON SLIME",
-    description: "Multi Drop – Fast Clear",
-    image: "assets/img/game_134.png",
-    script: ""
-  },
-  {
-    id: 135,
-    name: "HEROLL",
-    description: "Resources – Vip – Dump Enemy",
-    image: "assets/img/game_135.png",
-    script: ""
-  },
-  {
-    id: 136,
-    name: "HUNTER RAID IDLE RPG",
-    description: "Antiban – Free Shop – Unlock All Features",
-    image: "assets/img/game_136.png",
-    script: ""
-  },
-  {
-    id: 137,
-    name: "HUNTING SNIPPER",
-    description: "Damage",
-    image: "assets/img/game_137.png",
-    script: ""
-  },
-  {
-    id: 138,
-    name: "IDLE BERSERKER",
-    description: "Antiban – Free Shop – Unlock Equip – Unlimited Resources",
-    image: "assets/img/game_138.png",
-    script: ""
-  },
-  {
-    id: 139,
-    name: "LEGEND OF SLIME",
-    description: "Antiban – Free Shop – Key Dungeon – Unlock Character",
-    image: "assets/img/game_139.png",
-    script: ""
-  },
-  {
-    id: 140,
-    name: "LEGEND OF YEOMRA",
-    description: "Gold Drop – Free Shop",
-    image: "assets/img/game_140.png",
-    script: ""
-  },
-  {
-    id: 141,
-    name: "LUCIFER IDLE",
-    description: "Antiban – Free Shop",
-    image: "assets/img/game_141.png",
-    script: ""
-  },
-  {
-    id: 142,
-    name: "NGUYÊN TỐ 2D",
-    description: "Antiban – Diamond – Unlimited Resources",
-    image: "assets/img/game_142.png",
-    script: ""
-  },
-  {
-    id: 143,
-    name: "PIXEL HUNTER IDLE",
-    description: "Antiban – Hack Resources – Free Shop – Unlock Equip SSS",
-    image: "assets/img/game_143.png",
-    script: ""
-  },
-  {
-    id: 144,
-    name: "RAISING POSEIDON",
-    description:
-      "Antiban – Free Shop – Gold/Exp/Pearl Drop – Damage – Speed",
-    image: "assets/img/game_144.png",
-    script: ""
-  },
-  {
-    id: 145,
-    name: "SKULL GIRLS",
-    description: "Damage – Auto Win – Dump Enemy – No Skill CD",
-    image: "assets/img/game_145.png",
-    script: ""
-  },
-  {
-    id: 146,
-    name: "SLAYER LEGEND",
-    description:
-      "Antiban – Diamond – Gold/Exp Drop – Damage – God Mode",
-    image: "assets/img/game_146.png",
-    script: ""
-  },
-  {
-    id: 147,
-    name: "SLIME HUNTER WILD PATCH",
-    description: "Free Shop",
-    image: "assets/img/game_147.png",
-    script: ""
-  },
-  {
-    id: 148,
-    name: "UNKNOWN KNIGHT IDLE RPG",
-    description:
-      "Antiban – Rune x1000 – Hack Resources – No Skill CD – No Train Time",
-    image: "assets/img/game_148.png",
-    script: ""
-  },
-  {
-    id: 149,
-    name: "THE DEMONIZED IDLE RPG",
-    description: "Antiban – Free Shop – Exp + Gold Drop",
-    image: "assets/img/game_149.png",
-    script: ""
-  },
-  {
-    id: 150,
-    name: "VALKYRIE IDLE",
-    description: "Antiban – Unlimited Resources – Buy Costume",
-    image: "assets/img/game_150.png",
-    script: ""
-  },
-  {
-    id: 151,
-    name: "ZOMBIE KINGDOM IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_151.png",
-    script: ""
-  },
-  {
-    id: 152,
-    name: "AIRSHIP KNIGHT ZERO",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_152.png",
-    script: ""
-  },
-  {
-    id: 153,
-    name: "DEMON HUNTER IDLE RPG",
-    description: "Antiban – Free Shop – Speed Game",
-    image: "assets/img/game_153.png",
-    script: ""
-  },
-  {
-    id: 154,
-    name: "DRILL RUSH EX",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_154.png",
-    script: ""
-  },
-  {
-    id: 155,
-    name: "DULLAHA RPG",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_155.png",
-    script: ""
-  },
-  {
-    id: 156,
-    name: "GODCHER",
-    description: "Antiban – No ADS – Currency – Speed Game",
-    image: "assets/img/game_156.png",
-    script: ""
-  },
-  {
-    id: 157,
-    name: "HERO CREW",
-    description: "Antiban – Currency – Speed Game",
-    image: "assets/img/game_157.png",
-    script: ""
-  },
-  {
-    id: 158,
-    name: "HUNTER ORIDIN",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_158.png",
-    script: ""
-  },
-  {
-    id: 159,
-    name: "LEGEND OF AVATAR",
-    description: "Damage – Speed Game",
-    image: "assets/img/game_159.png",
-    script: ""
-  },
-  {
-    id: 160,
-    name: "LEGEND OF YEOMRA",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_160.png",
-    script: ""
-  },
-  {
-    id: 161,
-    name: "PET WAR IDLE RPG",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_161.png",
-    script: ""
-  },
-  {
-    id: 162,
-    name: "PIRATE SHIP",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_162.png",
-    script: ""
-  },
-  {
-    id: 163,
-    name: "RAISE YOUR KNIGHTLY ORDER IDLE",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_163.png",
-    script: ""
-  },
-  {
-    id: 164,
-    name: "THE KEEPER IDLE RPG",
-    description: "Free Shop – Speed Game",
-    image: "assets/img/game_164.png",
-    script: ""
-  },
-  {
-    id: 165,
-    name: "IDLE TRANG BỊ THẦN THOẠI",
-    description: "Freeshop – Speed Game",
-    image: "assets/img/game_165.png",
-    script: ""
-  },
-  {
-    id: 166,
-    name: "MA GIÁO HỒI QUY",
-    description: "Freeshop – EXP – Tốc độ di chuyển",
-    image: "assets/img/game_166.png",
-    script: ""
-  },
-  {
-    id: 167,
-    name: "BACCHUS: HIGH TENSION IDLE RPG",
-    description: "Freeshop",
-    image: "assets/img/game_167.png",
-    script: ""
-  },
-  {
-    id: 168,
-    name: "REAL CRICKET 20",
-    description: "MP – MC",
-    image: "assets/img/game_168.png",
-    script: ""
-  },
-  {
-    id: 169,
-    name: "HERO BLITZ",
-    description: "Freeshop",
-    image: "assets/img/game_169.png",
-    script: ""
-  },
-  {
-    id: 170,
-    name: "ZOMBIE KINGDOM IDLE RPG",
-    description: "Free Shop",
-    image: "assets/img/game_170.png",
-    script: ""
-  }
-];
